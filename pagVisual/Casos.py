f88b7a63a51896992c588719f8d13c4130f21301
JWinn01 <joshua.winn.2001@gmail.com>
2025-02-01 21:43:43 +0100

    arreglos

--- pagVisual/Casos.py
+++ pagVisual/Casos.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import Notebooks.modeling_final.modeloACO as maco
+import pagVisual.modeloACO as maco
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
